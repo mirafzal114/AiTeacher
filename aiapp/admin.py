88bb5a5cbfbf2cab5dbe393b251b1ebef53ffcbd
mirafzal114 <mirafzaldevelop@gmail.com>
2024-01-28 21:54:00 +0500

    admin.py changed

--- aiapp/admin.py
+++ aiapp/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from .models import FeedBack
 # Register your models here.
-admin.site.register(FeedBack)
+
+class FeedBackAdmin(admin.ModelAdmin):
+    list_display = ('user', 'feedback', 'date')
+    list_filter = ('user', 'date')
+    search_fields = ('user__username', 'feedback')
+    date_hierarchy = 'date'
+    ordering = ('-date',)
+
+    fieldsets = (
+        ('User Information', {'fields': ('user',)}),
+        ('Feedback Details', {'fields': ('feedback',)}),
+        ('Date Information', {'fields': ('date',)}),
+    )
+    readonly_fields = ('date',)
+
+admin.site.register(FeedBack, FeedBackAdmin)
