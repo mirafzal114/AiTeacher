cb2a632df3cd1caa048893a9fc3b29dbaa65ae7f
mirafzal114 <mirafzaldevelop@gmail.com>
2024-01-28 21:55:43 +0500

    view.py changed

--- aiapp/views.py
+++ aiapp/views.py
@@ -1,16 +1,17 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
 from django.contrib.auth.models import User
-from django.contrib import messages
-from aiapp.form import RegisterForm, FeedBackForm
+from aiapp.forms import RegisterForm, FeedBackForm  # Assuming your form is in a file named forms.py
 
 load_dotenv()
+
+
 def ielts_checking(txt):
     api_key = os.getenv("demokey")
-
     if not api_key:
         return "API key not found. Please set your OpenAI API key."
 
@@ -21,33 +22,34 @@ def ielts_checking(txt):
             {
                 "role": "user",
                 "content": f'''
-            check this essay 
-            {txt}
+                check this essay 
+                {txt}
 
-            answer about this essay like this :
-            your ielts writing score: (write an approximate ielts writing task2 grade for this essay.)
-            your mistakes:
-        
-            .......
+                answer about this essay like this :
+                your IELTS writing score: (write an approximate IELTS Writing Task 2 grade for this essay.)
+                your mistakes:
 
-            Here is a perfect example for your topic:
-            (write here perfect essay example)
-            
-            display the answer nicely with design html and css code 
-            kind off:
-    <h1 style="font-family: 'Lucida Grande', Arial, sans-serif">An approximate IELTS Writing Task 2 grade for this essay is '5.5'  &#127882</h1>
-    <h2 style="font-family: 'Lucida Grande', Arial, sans-serif">Your Mistakes &#128269;: </h2>
-    <ul class="mistakes">
-        <li>1 ....</li>
-        <li>2 ...</li>
-        <li>3...</li>
-        <li>4 ...</li>
-        <li>5 ...</li>
-    </ul>
-        <h2>Here is a perfect example for your topic: </h2>
-        <p>essay ..........</p>
-            make the design more beautiful beautiful 
-            '''}
+                .......
+
+                Here is a perfect example for your topic:
+                (write here a perfect essay example)
+
+                display the answer nicely with design HTML and CSS code 
+                kind of:
+                <h1 class="score">An approximate IELTS Writing Task 2 grade for this essay is '5.5' &#127882</h1>
+                <h2 class="mistakes-title">Your Mistakes &#128269;: </h2>
+                <ul class="mistakes">
+                    <li>1 ....</li>
+                    <li>2 ...</li>
+                    <li>3...</li>
+                    <li>4 ...</li>
+                    <li>5 ...</li>
+                </ul>
+                <h2>Here is a perfect example for your topic: </h2>
+                <p class="perfect-example">essay ..........</p>
+                make the design more beautiful
+                '''
+            }
         ],
         model="gpt-3.5-turbo",
     )
@@ -61,14 +63,12 @@ def ielts_checking_view(request):
         essay_text = request.POST.get('essay_text', '')
         result = ielts_checking(essay_text)
         result_end = result
-        return render(request, 'aiapp/essey.html', {'grade':result_end})
+        return render(request, 'aiapp/essay.html', {'grade': result_end})
     return render(request, 'aiapp/home.html')
-
 
 
 def dictionary(word):
     api_key = os.getenv("demokey")
-
     if not api_key:
         return "API key not found. Please set your OpenAI API key."
 
@@ -79,73 +79,71 @@ def dictionary(word):
             {
                 "role": "user",
                 "content": f'''
-give information about this word '{word}'
-answer like this 
-<style>
-    #definition,
-    #examples 
-        background-color: #f8f8f8;
-        padding: 15px;
-        margin-bottom: 20px;
-        border-radius: 8px;
-        box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
-    
+                give information about this word '{word}'
+                answer like this 
+                <style>
+                    #definition,
+                    #examples 
+                        background-color: #f8f8f8;
+                        padding: 15px;
+                        margin-bottom: 20px;
+                        border-radius: 8px;
+                        box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
 
-    h2 
-        color: #333;
-    
 
-    p 
-        color: #555;
-        line-height: 1.6;
-    
-    strong 
-        color: #007BFF;
-    
+                    h2 
+                        color: #333;
 
-    ol 
-        list-style-type: decimal;
-        padding-left: 20px;
-    
 
-    li 
-        margin-bottom: 8px;
-    
-</style>
+                    p 
+                        color: #555;
+                        line-height: 1.6;
 
-<h2>Definition:</h2>
-<div id="definition">
-    <p>"Find" is a verb that denotes the act of discovering or locating something, often as a result of searching,
-        exploring, or investigating.</p>
-</div>
+                    strong 
+                        color: #007BFF;
 
-<h2>Examples:</h2>
-<div id="examples">
-    <ol>
-        <li><strong>Discovering something:</strong> While hiking in the woods, I found a beautiful waterfall.</li>
-        <li><strong>Locating someone or something:</strong> I need to find my glasses before I can start reading.</li>
-        <li><strong>Realizing or identifying something:</strong> After years of searching, he finally found his true
-            passion in painting.</li>
-        <li><strong>Meeting someone unexpectedly:</strong> I didn't expect to find my sister at the coffee shop; it was
-            a pleasant surprise.</li>
-    </ol>
-</div>
-continue write more information as much as possible
-'''}
+                    ol 
+                        list-style-type: decimal;
+                        padding-left: 20px;
+
+                    li 
+                        margin-bottom: 8px;
+
+                </style>
+
+                <h2>Definition:</h2>
+                <div id="definition">
+                    <p>"Find" is a verb that denotes the act of discovering or locating something, often as a result of searching,
+                        exploring, or investigating.</p>
+                </div>
+
+                <h2>Examples:</h2>
+                <div id="examples">
+                    <ol>
+                        <li><strong>Discovering something:</strong> While hiking in the woods, I found a beautiful waterfall.</li>
+                        <li><strong>Locating someone or something:</strong> I need to find my glasses before I can start reading.</li>
+                        <li><strong>Realizing or identifying something:</strong> After years of searching, he finally found his true
+                            passion in painting.</li>
+                        <li><strong>Meeting someone unexpectedly:</strong> I didn't expect to find my sister at the coffee shop; it was
+                            a pleasant surprise.</li>
+                    </ol>
+                </div>
+                continue writing more information as much as possible
+                '''
+            }
         ],
         model="gpt-3.5-turbo",
     )
 
     response_content = chat_completion.choices[0].message.content
-
     return response_content
+
 
 def dict_view(request):
     if request.method == 'POST':
         word = request.POST.get('word', '')
         result = dictionary(word)
-        return render(request, 'aiapp/dictionary.html', {'result':result,
-                                                         'word':word})
+        return render(request, 'aiapp/dictionary.html', {'result': result, 'word': word})
     return render(request, 'aiapp/home.html')
 
 
@@ -153,23 +151,24 @@ def advice(topic):
     api_key = os.getenv("demokey")
     if not api_key:
         return "API key not found. Please set your OpenAI API key."
+
     client = OpenAI(api_key=api_key)
     chat_completion = client.chat.completions.create(
         messages=[
             {
                 "role": "user",
                 "content": f'''
-                write to this '{topic}' topic
-                more ideas for writing essay ..
-                write it on html like this 
+                write about this '{topic}' topic
+                more ideas for writing an essay ..
+                write it in HTML like this 
                 <h4> ......</h4>
-'''}
+                '''
+            }
         ],
         model="gpt-3.5-turbo",
     )
 
     response_content = chat_completion.choices[0].message.content
-
     return response_content
 
 
@@ -177,7 +176,7 @@ def advice_view(request):
     if request.method == 'POST':
         topic = request.POST.get('topic', '')
         result = advice(topic)
-        return render(request, 'aiapp/advice.html', {'result':result})
+        return render(request, 'aiapp/advice.html', {'result': result})
 
     return render(request, 'aiapp/home.html')
 
@@ -185,8 +184,10 @@ def advice_view(request):
 def about(request):
     return render(request, 'aiapp/about.html')
 
+
 def contact(request):
     return render(request, 'aiapp/contact.html')
+
 
 def service(request):
     return render(request, 'aiapp/service.html')
@@ -197,11 +198,13 @@ def register_view(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Registration successful. Please log in.')
             return redirect('login')
     else:
         form = RegisterForm()
 
     return render(request, 'aiapp/register.html', {'form': form})
+
 
 @login_required
 def feedback_view(request):
